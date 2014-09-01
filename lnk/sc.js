cde2c93b633ea3d677013cf4f92124c90801f85e
jazzart <jazzart>
2014-09-01 17:40:44 +0200

    jbsauto initial commit
    jbsauto initial commit
    1st site version

---
+++ lnk/sc.js
@@ -0,0 +1,30 @@
+var jbsapp = angular.module('jbs', ['ngRoute']); 
+
+jbsapp.config([ '$routeProvider',function ($routeProvider) {
+	$routeProvider
+	.when('/onas', {
+		templateUrl: '/tree/about.html', 
+		controller: 'main'
+	})
+	.when('/', {
+		templateUrl: '/tree/sg.html',
+		controller: 'main'		
+	})
+	.when('/uslugi', {
+		templateUrl: '/tree/service.html', 
+		controller: 'main'
+	})
+	.when('/aktualnosci', {
+		templateUrl: '/tree/news.html', 
+		controller: 'main'
+	})
+	.when('/kontakt', {
+		templateUrl: '/tree/contact.html', 
+		controller: 'main'
+	})
+}]).controller('main', function($scope) {
+	var a = "jbs-78@";
+	var b = "o2.pl";
+	$scope.adrs = a + b;
+	$scope.tel = "606 934 990";
+});
