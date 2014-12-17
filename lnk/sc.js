ee4f659421581f88f66ccd5d481d9da538003b3e
Krzysztof <jazzart.eu@gmail.com>
2014-12-17 22:01:58 +0100

    about page updates and moving index script
    about page updates and moving index script to controller

--- lnk/sc.js
+++ lnk/sc.js
@@ -28,8 +28,15 @@ jbsapp.config([ '$routeProvider',function ($routeProvider) {
 	$scope.adrs = a + b;
 	$scope.tel = "606 934 990";
 	$scope.place = "Janiszew 99/2";
+	$scope.town = "26-652 Zakrzew";
+
 	var $carousel = $("#carousel");
 	var $slides = $carousel.find("li");
+
+	var url = window.location.href;
+	var cut = url.slice(url.indexOf("#"));
+	var $body = $("body");
+	var navs = $("ul.main-nav li a");
 
 	$carousel.cycle({
 		slides: $slides, 
@@ -40,5 +47,38 @@ jbsapp.config([ '$routeProvider',function ($routeProvider) {
 		pager: ">.around > .cycle-pager"  
 	});
 
+	$("a.trackto").click(function (event) {
+		event.preventDefault();
+		var trg = $(event.target).attr("href");
+		$body.removeClass("short");
+		$(".contact").find(".active").removeClass("active");
+		$(".about").addClass("active");	
+		window.location.href = trg;
+	});
+
+
+	checkStyle(cut);
+	$.each(navs, function(i, el) {
+		var $el = $(el);
+		if ($el.attr("href") === cut) {
+			$el.addClass('active');
+		}
+	});
+	navs.click(function(e){
+		var t = $(this);
+		var href = $(e.target).attr("href");
+		t.addClass('active');
+		t.parent().siblings().find('a').removeClass("active");
+		checkStyle(href);
+	});
+
+
+	function checkStyle(cut) {
+		if (cut === "/" || cut === "#/" || cut ==="#/kontakt") {
+			$body.addClass("short");
+		} else {
+			$body.removeClass("short");
+		}
+	}
 
 });
